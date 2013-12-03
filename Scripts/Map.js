3164aaab3564502f6ec3b22bfc47677fd25d1684
Mark Pearl <markpearl@gmail.com>
2013-12-03 11:02:05 +0200

    Add scripts

---
+++ Scripts/Map.js
@@ -0,0 +1,26 @@
+﻿function initialize() {
+    
+    
+    var latlng = new google.maps.LatLng(-26.053717, 27.962527);
+    var myOptions = { zoom: 16, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    var map = new google.maps.Map(document.getElementById("streetmap"), myOptions);
+    var panoramaOptions = {
+        position: latlng,
+        pov: {
+            heading: 220,
+            pitch: 0,
+            zoom: 1
+        }
+    };
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: 'Villa Etrucia'
+    });
+    
+    var myStreetView = new google.maps.StreetViewPanorama(document.getElementById("streetview"), panoramaOptions);
+    myStreetView.setVisible(true);
+    map.setStreetView(myStreetView);
+
+}
---
+++ Scripts/Twitter.js
@@ -0,0 +1,29 @@
+﻿var Tweets = new TWTR.Widget({
+    version: 2,
+    type: 'search',
+    search: '#villaetrucia',
+    interval: 30000,
+    title: '#VillaEtrucia Tweets',
+    subject: '',
+    width: 750,
+    height: 125,
+    theme: {
+        shell: {
+            background: '#E7DCCA',
+            color: '#000000'
+        },
+        tweets: {
+            background: '#ffffff',
+            color: '#444444',
+            links: '#1985b5'
+        }
+    },
+    features: {
+        scrollbar: false,
+        loop: true,
+        live: true,
+        behavior: 'default'
+    }
+});
+
+Tweets.render().start();
